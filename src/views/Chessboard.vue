fe6ce85c1b6b6dd359c118a5b8c620912fe65fc6
vu-pa <vu.pa@akb.vn>
2024-03-12 21:32:09 +0700

    check winner

--- src/views/Chessboard.vue
+++ src/views/Chessboard.vue
@@ -23,23 +23,150 @@
 
 <script setup lang="ts">
     import { reactive, ref } from 'vue'
-    const arr = Array(20).fill(-1)
-    const chessboardData = reactive([])
-    for (let i = 0; i < 20; i += 1) {
-        const arr = [];
-        for (let j = 0; j < 20; j += 1) {
-            arr.push(-1);
-        }
-        chessboardData.push(arr);
-    }
+    import Swal from 'sweetalert2'
+
+    let chessboardData = reactive([])
     const xoFlag = ref(0); // X = 0, O = 1
+
+    const initChessboard = () => {
+        for (let i = 0; i < 20; i += 1) {
+            const arr = [];
+            for (let j = 0; j < 20; j += 1) {
+                arr.push(-1);
+            }
+            chessboardData.push(arr);
+        }
+    }
+
+    initChessboard();
+
+    const clearChestboard = () => {
+        for (let i = 0; i < 20; i += 1) {
+            for (let j = 0; j < 20; j += 1) {
+                chessboardData[i][j] = -1;
+            }
+        }
+    }
+
+    const showWinnerMessage = async (winner: string) => {
+        await Swal.fire({
+            title: "Congratulations!",
+            text: `${winner} is the winner`,
+            icon: "success"
+        });
+    }
     const handleClick = (i: number, j: number) => {
-        console.log(i, j)
         if (chessboardData[i - 1][j - 1] === -1) {
             chessboardData[i - 1][j - 1] = xoFlag.value;
+            checkWinner(i - 1, j - 1);
             xoFlag.value = (xoFlag.value ? 0 : 1);
         }
-        console.log(chessboardData)
+    }
+    const checkWinner = async (rowIndex: number, colIndex: number) => {
+        const xo = chessboardData[rowIndex][colIndex];
+
+        let adjacentCellInRow = 0;
+        for (let j = colIndex + 1; j < 20; j += 1) {
+            if (chessboardData[rowIndex][j] === xo) {
+                adjacentCellInRow += 1;
+            }
+            else {
+                break;
+            }
+        }
+        for (let j = colIndex - 1; j >= 0; j -= 1) {
+            if (chessboardData[rowIndex][j] === xo) {
+                adjacentCellInRow += 1;
+            }
+            else {
+                break;
+            }
+        }
+        if (adjacentCellInRow === 4) {
+            console.log(`${xo ? 'O' : 'X'} won`);
+            await showWinnerMessage(`${xo ? 'O' : 'X'}`)
+            // clearChestboard();
+        }
+
+        let adjacentCellInCol = 0;
+        for (let i = rowIndex + 1; i < 20; i += 1) {
+            if (chessboardData?.[i]?.[colIndex] === xo) {
+                adjacentCellInCol += 1;
+            }
+            else {
+                break;
+            }
+        }
+        for (let i = rowIndex - 1; i >= 0; i -= 1) {
+            if (chessboardData?.[i]?.[colIndex] === xo) {
+                adjacentCellInCol += 1;
+            }
+            else {
+                break;
+            }
+        }
+        if (adjacentCellInCol === 4) {
+            console.log(`${xo ? 'O' : 'X'} won`);
+            await showWinnerMessage(`${xo ? 'O' : 'X'}`)
+            // clearChestboard();
+        }
+
+        let adjacentCellInFirstDiagonal = 0;
+        let i = rowIndex;
+        let j = colIndex;
+        while (i < 20 && j < 20) {
+            if (chessboardData[--i][--j] === xo) {
+                adjacentCellInFirstDiagonal += 1;
+            }
+            else {
+                break;
+            }
+        }
+        i = rowIndex;
+        j = colIndex;
+        while (i < 20 && j < 20) {
+            if (chessboardData[++i][++j] === xo) {
+                adjacentCellInFirstDiagonal += 1;
+            }
+            else {
+                break;
+            }
+        }
+        if (adjacentCellInFirstDiagonal === 4) {
+            console.log(`${xo ? 'O' : 'X'} won`);
+            await showWinnerMessage(`${xo ? 'O' : 'X'}`)
+            // clearChestboard();
+        }
+
+        let adjacentCellInSecondDiagonal = 0;
+        i = rowIndex;
+        j = colIndex;
+        while (i < 20 && j < 20) {
+            if (chessboardData[++i][--j] === xo) {
+                adjacentCellInSecondDiagonal += 1;
+            }
+            else {
+                break;
+            }
+        }
+        i = rowIndex;
+        j = colIndex;
+        while (i < 20 && j < 20) {
+            if (chessboardData[--i][++j] === xo) {
+                adjacentCellInSecondDiagonal += 1;
+            }
+            else {
+                break;
+            }
+        }
+        if (adjacentCellInSecondDiagonal === 4) {
+            console.log(`${xo ? 'O' : 'X'} won`);
+            await showWinnerMessage(`${xo ? 'O' : 'X'}`)
+            // clearChestboard();
+        }
+        console.log('adjacentCellInRow: ', adjacentCellInRow)
+        console.log('adjacentCellInCol', adjacentCellInCol)
+        console.log('adjacentCellInD', adjacentCellInFirstDiagonal)
     }
 </script>
 
