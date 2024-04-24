fe7713e2f576f3d5559007e6b04ad184a29c445c
vu-pa <vu.pa@akb.vn>
2024-04-24 10:24:44 +0700

    change path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Game from '@/views/game/Game.vue';
-import Home from '@/views/home/Home.vue';
+import Game from '../views/game/Game.vue';
+import Home from '../views/home/Home.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
